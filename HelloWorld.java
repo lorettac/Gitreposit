bb31bd65ae025421d2ebef2e1143ea972ae5218f
loretta <lcobbson@hotmail.co.uk>
2013-09-13 11:14:38 +0000

    version2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,11 +2,11 @@ public class HelloWorld {
 	public static void main (String[] args) 
 	{
 		//declare and initialize name
-		String name="Akua";
+		String name="Abena";
 		//declare and initialize age
-		int age=19;
+		int age=20;
 		//declare and initialize height
-		double height=1.6;
+		double height=1.8;
 
 		//make a print statement to write the sentence with the 
 		//variables plugged into the sentence	
