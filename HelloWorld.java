8c83d3688378f314b83e0cb70ce4d1be21e0d2a6
loretta <lcobbson@hotmail.co.uk>
2013-09-13 10:26:20 +0000

    assignment

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+	public static void main (String[] args) 
+	{
+		//declare and initialize name
+		String name="Akua";
+		//declare and initialize age
+		int age=19;
+		//declare and initialize height
+		double height=1.6;
+
+		//make a print statement to write the sentence with the 
+		//variables plugged into the sentence	
+		System.out.println("Hello" +name+ "!you are"    +age+"and you are" +height+ "tall");
+	}
+}	
